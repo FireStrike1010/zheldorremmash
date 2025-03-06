51ff2ce737184f109f1c22ccd4e8ee8b77791d9c
FireStrike1010 <81763537+FireStrike1010@users.noreply.github.com>
2025-03-06 22:04:44 +0300

    Delete app directory

--- app/blueprints/base.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, session, render_template
-from datetime import datetime
-
-
-base = Blueprint('base', __name__, template_folder='../templates', static_folder='../static')
-
-@base.route('/', methods=['GET'])
-def index():
-    return render_template('base/index.html', session=session, year=datetime.now().year)
